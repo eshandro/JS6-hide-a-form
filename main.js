924f0316dbfcd4bb1dcd54fa84cd3c0901782e48
Erik Shandrowski <erik843@gmail.com>
2014-06-27 17:07:42 -0600

    all done but bonus

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $(document).on('ready', function() {
-  
+  	$('.enter-profile').submit(function(e) {
+	  	e.preventDefault();
+	  	$('.user-name').html($('.form-user-name').val());
+	  	$('.page-title').html($('.form-user-name').val() + '\'s Profile Page');
+
+	  	$('.bio').html($('.form-user-bio textarea').val());
+
+	  	$('.books ul').append('<li>' + $('.form-books1').val() + '</li>');
+	   	$('.books ul').append('<li>' + $('.form-books2').val() + '</li>');
+
+	    $('.js-libraries ul').append('<li>' + $('.form-js-libraries1').val() + '</li>');
+	   	$('.js-libraries ul').append('<li>' + $('.form-js-libraries2').val() + '</li>'); 	
+  });
+  	
+  	$('.show-hide-form').on('click', function() {
+  		$('.enter-profile').toggleClass('is-hidden');
+  		if($(this).html() === 'Enter profile info') {
+  			$(this).html('Hide the Form');
+  		}
+  		else {
+  			$(this).html('Enter profile info');
+  		}
+  	});
+
+  	$('.form-user-name').on('blur', function() {
+  		$('.user-name').html($(this).val());
+  		
+  	});
 });
